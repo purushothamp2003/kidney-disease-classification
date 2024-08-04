615d93b60087583c013135ef5209003ef3f602e3
purushothamp2003 <2021cs_purushothamp_b@nie.ac.in>
2024-08-05 02:13:12 +0530

    Base model added to pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +8,19 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>{STAGE_NAME} started <<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f" >>> stage {STAGE_NAME} completed <<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model Downloading stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>{STAGE_NAME} started <<<")
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f" >>> stage {STAGE_NAME} completed <<<")
     except Exception as e:
