76d9f740470aa23f0032ae1b11b82227566935fb
purushothamp2003 <2021cs_purushothamp_b@nie.ac.in>
2024-08-07 17:05:37 +0530

    Dvc added

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import mlflow
 import mlflow.keras
 from urllib.parse import urlparse
-from src.cnnClassifier.entity.config_entity import EvaluationConfig
+from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import read_yaml, create_directories, save_json
 import os
 
